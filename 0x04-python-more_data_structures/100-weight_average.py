4afbde3c94afcbc2992255ae9c0a57e8f683d3e3
Jesse Ntow <Jessentow@gmail.com>
2024-01-10 21:51:13 -0800

    100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+        digit1 = 0
+        digit2 = 0
+
+        for tup in my_list:
+        digit1 += tup[0] * tup[1]
+        digit2 += tup[1]
+
+        return (digit1 / digit2)
