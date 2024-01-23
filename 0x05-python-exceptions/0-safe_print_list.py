5e296f2fecd53f15c85e877be11afdd92eb2e3a1
Jesse Ntow <Jessentow@gmail.com>
2024-01-23 02:31:41 -0800

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    jes = 0
+    for a in range(x):
+        try:
+            print("{}".format(my_list[a]), end="")
+            jes += 1
+        except IndexError:
+            break
+    print("")
+    return (jes)
