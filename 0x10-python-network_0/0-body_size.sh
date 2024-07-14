d1d02c111bc51d19981a882dc7578f4abac56aa0
Jesse Ntow <Jessentow@gmail.com>
2024-07-14 13:34:18 -0700

    python network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# sends a request an URL, and displays the size of the body of the response
+#sends a request an URL, and displays 
+#the size of the body of the response
 curl -s "$1" | wc -c
